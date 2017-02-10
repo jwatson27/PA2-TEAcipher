338cae1bcc81b3ecd6db3d537237d160f5a276f3
Benjamin Ratkey <benratkey@gmail.com>
2017-02-10 08:31:31 -0500

    Added src files provided by Dr. Hamman

---
+++ TinyE.java
@@ -0,0 +1,15 @@
+package edu.cedarville.cs.crypto;
+
+public class TinyE {
+	
+	public static enum Mode { ECB, CBC, CTR };
+	
+	public Integer[] encrypt(Integer[] plaintext, Integer[] key, Mode mode, Integer[] iv) {
+		return null;
+	}
+
+	public Integer[] decrypt(Integer[] ciphertext, Integer[] key, Mode mode, Integer[] iv) {
+		return null;
+	}
+	
+}
