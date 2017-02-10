3c4f0afde51d335d014a0aaaf7bdc5d095c3fea0
jwatson27 <jwatson27@163.11.238.213>
2017-02-10 08:58:04 -0500

    Moved main.java into nb project

---
+++ teaCipher/src/edu/cedarville/cs/crypto/Tools.java
@@ -0,0 +1,21 @@
+package edu.cedarville.cs.crypto;
+
+public class Tools {
+	
+	public static Integer[] convertFromBytesToInts(byte[] bs) {
+		return null;
+	}
+	
+	public static Integer[] convertFromHexStringToInts(String s) {
+		return null;
+	}
+	
+	public static byte[] convertFromIntsToBytes(Integer[] ints) {
+		return null;
+	}
+	
+	public static String convertFromIntsToHexString(Integer[] ints) {
+		return null;
+	}
+	
+}
