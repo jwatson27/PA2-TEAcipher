f27d9dfa4624f089bd99a8ec9c327e4a515a7e7a
Benjamin Ratkey <benratkey@gmail.com>
2017-02-12 21:29:50 -0500

    Added outer For loop to TinyE.decrypt()

--- teaCipher/src/edu/cedarville/cs/crypto/TinyE.java
+++ teaCipher/src/edu/cedarville/cs/crypto/TinyE.java
@@ -20,7 +20,6 @@ public class TinyE {
             for (int j = 0; j < plaintext.length; j++) {
                         
                 // FIXME: get first block (64 bits)
-                // FIXME: Handle padding (if needed) know when to handle
                 left = plaintext[j];
                 right = plaintext[j+1];                                                                
                 
@@ -40,22 +39,30 @@ public class TinyE {
 	}
 
 	public Integer[] decrypt(Integer[] ciphertext, Integer[] key, Mode mode, Integer[] iv) {
-                // FIXME: maybe move these two lines inside the conditionals?
+            
+            //define variables
+            int left, right, sum;
+            Integer[] plaintext = null;
+            for (int j = 0; j < ciphertext.length; j++) {
+                        
+                // FIXME: get first block (64 bits)
+                left = ciphertext[j];
+                right = ciphertext[j+1];                                                                
+                
                 // FIXME: for now, assuming that ciphertext is exatcly 64 bits (1 block, two 32-bit halves)
-                int left = ciphertext[0];
-                int right = ciphertext[1];
-                Integer[] plaintext = null;
+                //int left = ciphertext[0];
+                //int right = ciphertext[1];
                 if (mode == Mode.ECB) {
                     // Electronic Codebook mode
                     // 
-                    int sum = delta << 5;
+                    sum = delta << 5;
                     for ( int i = 0; i < 32; i++ ) {
                         right = right - ((( left << 4) + key[2]) ^ ( left + sum) ^ (( left >> 5) + key[3]));
                         left  =  left - (((right << 4) + key[0]) ^ (right + sum) ^ ((right >> 5) + key[1]));
                         sum = sum - delta;
                     }
-                    plaintext[0] = left;
-                    plaintext[1] = right;
+                    plaintext[j] = left;
+                    plaintext[j + 1] = right;
                 }
                 else if (mode == Mode.CBC) {
                     // Cipher Block Chaining mode
@@ -64,7 +71,8 @@ public class TinyE {
                 else {
                     // CTR mode
                 }
-		return plaintext;
+            }
+            return plaintext;
 	}
 	
 }
